5c73e537e0e66fb049bd9acb8d0019f6fbf46836
KABORE B STEPHANE <uukiah90gmail.com>
2026-04-12 21:56:14 +0000

    Contournement du pare-feu Senfenico

--- api/payment_init.js
+++ api/payment_init.js
@@ -14,6 +14,7 @@ export default async function handler(req, res) {
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
         // La clé secrète est injectée automatiquement par Vercel !
         'X-API-KEY': process.env.SENFENICO_API_KEY, 
       },
